98c1eeaf7d23963fa50138ec0d93c235a8f8850b
wei-coding <edward.yu0830@gmail.com>
2021-06-24 19:09:04 +0800

    6/24 rust練習 第2章

---
+++ ch1/README.md
@@ -0,0 +1,21 @@
+# CH1 Starter
+
+重點整理
+
+## CH1.1
+
+安裝說明（略）
+
+## CH1.2
+
+* Rust 程式碼以 `.rs` 作為檔名
+* compile：`rustc path_to_src.rs`
+
+## CH1.3
+
+* cargo 用來管理專案
+* 使用 `cargo new project` 新建專案
+* `Cargo.toml`：敘述專案內容的檔案
+* `cargo build`
+* `cargo run`
+* `cargo check`：用於檢查是否可編譯，但實際上不編譯（較快）
---
+++ ch2/guessing_game/src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess a number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("secret_number is {}", secret_number);
+
+    loop {
+        println!("enter a number:");
+
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error when readline");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("the number you entered is {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Win!!");
+                break;
+            }
+        };
+    }
+}
